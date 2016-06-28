9282fbd1b98dc34615bfee7459547e36acea578d
peterhwang2010 <inhohwang2010@gmail.com>
2016-06-27 23:39:19 -0400

    connected to mlab

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('config');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
+var dbConfig = config.get('User.dbConfig');
+var dbURL = 'mongodb://' + dbConfig.username + ':' + dbConfig.password + '@ds023644.mlab.com:23644/demylee';
+
+mongoose.connect(dbURL);
 
 var port = process.env.PORT || 8080;
 
@@ -16,4 +23,4 @@ router.get('/', function(req, res){
 app.use('/api', router);
 
 app.listen(port);
-console.log('Listening to pot ' + port);
+console.log('Listening to port ' + port);
